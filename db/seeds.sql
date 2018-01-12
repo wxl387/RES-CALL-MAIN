a77ad86dcc6020d92e2048ba396154002fbe6b19
Wenbiao Li <lixuyao.tony@gmail.com>
2018-01-12 10:47:20 -0500

    database update

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,20 +1,23 @@
 INSERT INTO scenarios ( 
   body_part,
   body_part_specific,
-  treatment
+  plan
   )
   VALUES
-    ("Femur", "Proximal", "Fix it!"),
-    ("Femur", "Midshaft", "Fix it!"),
-    ("Femur", "Distal", "Fix it!");
+    ("Default", "", ""),
+    ("Femur", "Proximal femur fracture", "AP pelvis, lateral hip and Judet views (both pre and post-reduction) || inlet and outlet views (if acetabular or pelvic ring injury suspected) || CT after reduction || Get consent || Pre-operative medicine clearance"),
+    ("Femur", "Midshaft femur fracture", "AP/lateral of entire femur || AP/lateral of ipsilateral hip || AP/lateral of ipsilateral knee || Call attending on staff || Get consent || Pre-operative medicine clearance"),
+    ("Femur", "Distal femur fracture", "Standard AP/Lateral || Traction AP/Lateral/oblique || Call attending on staff || Get consent || Pre-operative medicine clearance!");
 
 INSERT INTO patients ( 
   patient_name,
   MRN,
-  HPI
+  injury,
+  HPI,
+  plan
   )
   VALUES
-    ("Chang", "0207890", "31YO M w/ too much work."),
-    ("Heejin", "3987105", "29YO F w/ lots of love."),
-    ("Mom", "0298720", "55YO F who is great."),
-    ("Pop", "1598023", "61YO M who is also great.")
+    ("Chang", "0207890", "Proximal femur fracture", "31YO M w/ too much work.", ""),
+    ("Heejin", "3987105", "Proximal femur fracture", "29YO F w/ lots of love.", ""),
+    ("Mom", "0298720", "Distal femur fracture", "55YO F who is great.", ""),
+    ("Pop", "1598023", "Distal femur fracture", "61YO M who is also great." ,"")
--- public/assets/javascript/javascript.js
+++ public/assets/javascript/javascript.js
@@ -1,9 +1,23 @@
 $(document).ready(function() {
+
 	$("#add-patient").on("click", function(event) {
 		$.get("/getAllInjuries", function(data) {
 			console.log("Injuries retrieved successfully!")
 		});
 	});
+	
+	$('#add-initial-task').keypress(function(e) {
+        if(e.which == 13) {
+            var task = $('#add-initial-task').val();
+            var checkBox = $("<input>");
+			var checkBoxStatus = "not_checked";
+			checkBox.attr({"type": "checkbox", "id": task, "checkbox_status": checkBoxStatus})
+			$("#patient-plan").append("<br>");
+			$("#patient-plan").append(checkBox);
+			$("#patient-plan").append(task);
+			$('#add-initial-task').val("");
+        };
+    });
 
 	$("#view-patients").on("click", function(event) {
 		$.get("/viewPatients", function(data) {
@@ -11,16 +25,42 @@ $(document).ready(function() {
 		});
 	});
 
-	console.log(document.querySelector('.patient-button'));
-
-	$(document).find(".patient-button").on("click", function(event) {
-		console.log(event);
-		var PID = event.target.id
+	$(".patient-button").on("click", function(event) {
+		console.log("You clicked on a patient!")
+		console.log(event.target.id)
+		// var PID = event.target.id.split("-")[1];
+		var PID = event.target.id;
+		console.log(PID)
 		$.get("/viewPatients/"+PID, function(data) {
-			data = data.patientInfo["0"]
+			console.log(data)
+			data = data.patientInfo["0"];
 			$("#table-name").val(data["patient_name"]);
 			$("#table-MRN").val(data["MRN"]); 
-			$("#table-HPI").val(data["HPI"]); 
+			$("#table-injury").val(data["injury"]);
+			$("#table-HPI").val(data["HPI"]);
+			$("#table-plan").empty();
+			var planObj = JSON.parse(data["plan"]);
+			for (var action in planObj) {
+				var checkBox = $("<input>")
+				var checkBoxStatus = "";
+				checkBox.attr({"type": "checkbox", "id": action+"-"+PID, "checkbox_status": checkBoxStatus, "PID": PID, "action": action})
+				if (planObj[action] === "0") {
+					checkBox.attr("checkbox_status", "not_checked");
+					checkBox.prop("checked", false)
+				} else if (planObj[action] === "1") {
+					checkBox.attr("checkbox_status", "checked");
+					checkBox.prop("checked", true); 
+				};
+				$("#table-plan").append("<br>");
+				$("#table-plan").append(checkBox);
+				$("#table-plan").append(action);
+			};
+			
+			var addTask = $("<input>")
+			addTask.attr({"type": "text-field", "class": "form-control", "id": "add-update-task", "value": "", "PID": PID});
+			$("#add-update-task-span").empty();
+			$("#add-update-task-span").append("<br>Add task: <br>");
+			$("#add-update-task-span").append(addTask);
 		});
 		var updateButton = $("<button>")
 		updateButton.attr({"type": "button", "class": "btn btn-default update-patient-info", "id": "update_"+PID})
@@ -35,15 +75,84 @@ $(document).ready(function() {
 		$("#update-patient-button-div").append(deleteButton);		
 	});
 
-	$("#submit-patient").on("click", function() {				
+	$("#table-plan").on("click", function(event) {
+		var actionID = event.target.id;
+		if ($("[id='"+actionID+"']").attr("checkbox_status") === "not_checked") {
+			$("[id='"+actionID+"']").attr("checkbox_status", "checked")
+		} else if ($("[id='"+actionID+"']").attr("checkbox_status") === "checked") {
+			$(("[id='"+actionID+"']")).attr("checkbox_status", "not_checked")
+		}
+	});
+
+	$('#add-update-task-span').keypress(function(e) {
+        if(e.which == 13) {
+        	var PID = $('#add-update-task').attr("PID")
+            var task = $('#add-update-task').val();
+            var checkBox = $("<input>");
+			var checkBoxStatus = "not_checked";
+			checkBox.attr({"type": "checkbox", "id": task+"-"+PID, "checkbox_status": checkBoxStatus, "PID": PID, "action": task})
+			$("#table-plan").append("<br>");
+			$("#table-plan").append(checkBox);
+			$("#table-plan").append(task);
+        };
+    });
+
+	$('#injury-type').change(function(event) {
+	    var injury = event.target.value;
+	    console.log(injury)
+	    $.get("/getPlan/"+injury, function(data) {
+			var taskArray = data["plan"]["0"]["plan"].split(" || ");
+			for (var i = 0; i < taskArray.length; i++) {
+				var checkBox = $("<input>")
+				var checkBoxStatus = "not_checked";
+				checkBox.attr({"type": "checkbox", "id": taskArray[i], "checkbox_status": checkBoxStatus})
+				$("#patient-plan").append("<br>");
+				$("#patient-plan").append(checkBox);
+				$("#patient-plan").append(taskArray[i]);
+			};
+		});
+	});
+
+	$("#patient-plan").on("click", function(event) {
+		var actionID = event.target.id;
+		if ($("[id='"+actionID+"']").attr("checkbox_status") === "not_checked") {
+			$("[id='"+actionID+"']").attr("checkbox_status", "checked")
+		} else if ($("[id='"+actionID+"']").attr("checkbox_status") === "checked") {
+			$(("[id='"+actionID+"']")).attr("checkbox_status", "not_checked")
+		}
+	});
+
+	$("#submit-patient").on("click", function(event) {				
+	  	
+		var planObj = {};
+		$("#patient-plan input").each(function() {
+   			var action = $(this).attr("id");
+   			console.log(action)
+   			if ($(this).attr("checkbox_status") === "checked") {
+	   			planObj[action] = "1";
+	   		} else if ($(this).attr("checkbox_status") === "not_checked") {
+	   			planObj[action] = "0";
+	   		};
+		});
+
+		console.log(planObj)
+
 	  	var newPatient = {
 	  		"patient_name": $("#patient-name").val(),
 	  		"MRN": $("#patient-MRN").val(),
+	  		"injury": $("#injury-type").val(),
 	  		"HPI": $("#patient-HPI").val(),
+	  		"plan": JSON.stringify(planObj)
 	  	};
 
 	  	$.post("/addPatient/submitPatient", newPatient, function(data){
 	  		console.log("patient added successfully!")
+	  		$("#patient-name").val(""),
+	  		$("#patient-MRN").val(""),
+	  		$("#injury-type").val(""),
+	  		$("#patient-HPI").val(""),
+	  		$("#add-update-task").val("")
+	  		$("#patient-plan").empty()	
 	  	});
 	});
 
@@ -52,15 +161,27 @@ $(document).ready(function() {
 		var selectedButtonType = event.target.id.split("_")[0];
 		var PID = event.target.id.split("_")[1];
 		
+		var updatedPlan = {};
+		$("#table-plan input").each(function() {
+   			var action = $(this).attr("action");
+   			if ($(this).attr("checkbox_status") === "checked") {
+	   			updatedPlan[action] = "1";
+	   		} else if ($(this).attr("checkbox_status") === "not_checked") {
+	   			updatedPlan[action] = "0";
+	   		};
+		});
+
 		if (selectedButtonType === "update") {
 			var updatePatient = {
 				"id": PID,
 		  		"patient_name": $("#table-name").val(),
 		  		"MRN": $("#table-MRN").val(),
 		  		"HPI": $("#table-HPI").val(),
+		  		"plan": JSON.stringify(updatedPlan)
 		  	};
 
 		  	$.post("/viewPatients/updatePatient", updatePatient, function(data) {
+		  		$("#add-update-task").val("");
 		  		console.log("Patient updated successfully!")
 		  	});
 		};
@@ -70,8 +191,7 @@ $(document).ready(function() {
 		  	$.post("/viewPatients/deletePatient", {"id": PID}, function(data) {
 		  		console.log("Patient deleted successfully!")
 		  	});
+		  	$("#"+PID).remove()
 		};		
-
 	});
-
 });
