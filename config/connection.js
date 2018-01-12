f0f0092a5ff5f959ed3098323c0ece9dc64f3684
Wenbiao Li <lixuyao.tony@gmail.com>
2018-01-12 02:20:47 -0500

    jawsDB connection and schema set up in workbench

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection("");
+var connection = mysql.createConnection("mysql://sc6pssmesql2veha:yaqwru910enqhivp@izm96dhhnwr2ieg0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/k9hjz95uqz6gonet");
 
 // if (process.env.JAWSDB_URL) {
 //   var connection = mysql.createConnection(process.env.JAWSDB_URL);
